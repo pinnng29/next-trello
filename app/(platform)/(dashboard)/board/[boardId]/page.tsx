251d383d489ff25488d194b39f033f556e5a93f4
pinnng29 <apingspeedholic@gmail.com>
2024-03-14 03:58:51 +0700

    list options

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,9 +1,48 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
 
-
-export default function BoardIdPage() {
-  return (
-    <div>
-      Board ID!
-    </div>
-  )
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
 }
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer
+        boardId={params.boardId}
+        data={lists}
+      />
+    </div>
+  );
+};
+
+export default BoardIdPage;
